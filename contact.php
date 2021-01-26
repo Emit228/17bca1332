5a56b47eeab260675cb02353c07830f43bea17d3
Emit228 <57714878+Emit228@users.noreply.github.com>
2021-01-26 16:02:21 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php  require("header.php"); ?>
+<script type="text/javascript">
+	document.getElementById("acontact").className="active";
+</script>
+
+
+	<h1>Contact Us</h1>
+    <table>
+    
+	
+    <tr><td rowspan="4"><img src="ups/satish.jpg" style='height:100px; width:100px';></td><td>Name</td><td>:</td><td></td></tr>
+    <tr><td rowspan="1">Contact No</td><td>:</td><td></td></tr>
+    <tr><td rowspan="1">E-Mail Add</td><td>:</td><td></td></tr>
+   <tr><td rowspan="1">Website</td><td>:</td><td></td></tr>
+    </table>
+    
+<?php require("footer.php"); ?>
